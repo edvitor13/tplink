2de54fadd3bc5a476d5a35e85ca779f23b6fca7a
Vitor Gabriel <edvitor13@hotmail.com>
2024-06-22 15:42:22 -0300

    Criando setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='tplink',
+    version='0.1.0b1',
+    author='Vitor Gabriel',
+    author_email='edvitor13@hotmail.com',
+    packages=['tplink'],
+    python_requires='>=3.11',
+    install_requires=[
+        'python-kasa>=0.6.2.1'
+    ],
+)
